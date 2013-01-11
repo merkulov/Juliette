6f6adf1695fc704a0a91253cfc19db8eb266aab5
Mikhail Merkulov <m.merkulov@gmail.com>
2013-01-11 18:02:36 +0400

    Forgot to commit all files

---
+++ Juliette.Examples.MortgageCalculator/Models/OptionsModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Juliette.Examples.MortgageCalculator.Models
+{
+	public class OptionsModel
+	{
+		public OptionsModel()
+		{
+			Options = new List<Option>();
+		}
+
+		public List<Option> Options { get; private set; }
+	}
+	
+	public class Option
+	{
+		public int OrdinalNumber { get; set; }
+		public int NumberOfMonths { get; set; }
+		public decimal TotalPayment { get; set; }
+		public decimal MonthlyPayment { get; set; }
+		public decimal Interest { get; set; }
+	}
+}
